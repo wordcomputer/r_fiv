e09c3388424a318988f9738795cbe0723f4796ee
wordcomputer <120311743+wordcomputer@users.noreply.github.com>
2020-02-11 20:33:38 +0000

    Ajout de 1 annonce(s)

---
+++ CoinTap/Controller/GameOverViewController.swift
@@ -0,0 +1,17 @@
+
+//
+//  GameOverViewController.swift
+//  CoinTap
+//
+//  Created by Ridwan Abdurrasyid on 19/09/19.
+//  Copyright © 2019 Mentimun Mulus. All rights reserved.
+//
+
+import UIKit
+import HealthKitUI
+import HealthKit
+
+
+class GameOverViewController: UIViewController, HealthKitDelegate {
+    
+    @IBOutlet weak var closeButtonOutlet: UIButton!
