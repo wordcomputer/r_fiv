d1c8764a7cf9426bdcb72e64520fae791053f065
wordcomputer <120311743+wordcomputer@users.noreply.github.com>
2020-04-18 19:47:33 +0000

    Updated LICENSE and README

--- CoinTap/Controller/HitController.swift
+++ CoinTap/Controller/HitController.swift
@@ -36,4 +36,4 @@ extension ViewController : ARSessionDelegate {
         let offsetY : Float = 1
         let offsetZ : Float = 1
         
-        if vector1.x - offsetX < vecto
+        if vector1.x - offsetX < vector2.x && vector2.x < v
