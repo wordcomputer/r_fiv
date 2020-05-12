2e88cde641a7b0844cde84b2a290a0ab797bfdf4
wordcomputer <120311743+wordcomputer@users.noreply.github.com>
2020-05-12 18:40:37 +0000

    gitk: Use an external icon file on Windows

--- CoinTap/Controller/MultiplayerController.swift
+++ CoinTap/Controller/MultiplayerController.swift
@@ -38,3 +38,15 @@ extension ViewController : MCSessionDelegate {
     
     func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
         
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        
+    }
+    
+}
+
