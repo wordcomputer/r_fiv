4fbb5f4a33ae06cc3843cfa9b1265e742a2909fb
wordcomputer <120311743+wordcomputer@users.noreply.github.com>
2020-05-12 15:44:11 +0000

    Now it no longer crashes if X

--- CoinTap/Controller/MultiplayerController.swift
+++ CoinTap/Controller/MultiplayerController.swift
@@ -11,3 +11,30 @@ import SceneKit
 import ARKit
 import LocalAuthentication
 import MultipeerConnectivity
+
+extension ViewController : MCSessionDelegate {
+//    
+//    func startMultiplayer(){
+//        let myPeerID = MCPeerID.init(displayName: "Ridwan")
+//        let multiplayerSession = MCSession(peer: myPeerID, securityIdentity: nil, encryptionPreference: .required)
+//        multiplayerSession.delegate = self
+//
+//        serviceAdvertiser = MCNearbyServiceAdvertiser(peer: myPeerID, discoveryInfo: nil, serviceType: MultipeerSession.serviceType)
+//        serviceAdvertiser.delegate = self
+//        serviceAdvertiser.startAdvertisingPeer()
+//
+//        serviceBrowser = MCNearbyServiceBrowser(peer: myPeerID, serviceType: MultipeerSession.serviceType)
+//        serviceBrowser.delegate = self
+//        serviceBrowser.startBrowsingForPeers()
+//    }
+//    
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        
