be5bd9a85fc569335275f27af2f08fd7f6ee481d
wordcomputer <120311743+wordcomputer@users.noreply.github.com>
2020-05-27 16:20:22 +0000

    missing file.

--- CoinTap/Controller/TimerController.swift
+++ CoinTap/Controller/TimerController.swift
@@ -20,3 +20,4 @@ extension ViewController {
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
     
+    func timeString
