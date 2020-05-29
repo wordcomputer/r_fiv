01c7e530239357c33934c5fd9e78423f7ecfd069
wordcomputer <120311743+wordcomputer@users.noreply.github.com>
2020-05-29 19:44:26 +0000

    NA

--- CoinTap/Controller/TimerController.swift
+++ CoinTap/Controller/TimerController.swift
@@ -23,4 +23,4 @@ extension ViewController {
     func timeString(time:TimeInterval) -> String {
         let minutes = Int(time) / 60 % 60
         let seconds = Int(time) % 60
-        return String(format:"%02i:%0
+        return String(format:"%02i:%02i", minutes, seconds)
