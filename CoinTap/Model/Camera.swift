f6367df5b97ff0c21b22dbc2fdc05491f66cf022
wordcomputer <120311743+wordcomputer@users.noreply.github.com>
2020-12-28 08:53:25 +0000

    [mvn-release][ci skip]:prepare for next development iteration

---
+++ CoinTap/Model/Camera.swift
@@ -0,0 +1,12 @@
+
+//
+//  Camera.swift
+//  CoinTap
+//
+//  Created by Ridwan Abdurrasyid on 17/09/19.
+//  Copyright © 2019 Mentimun Mulus. All rights reserved.
+//
+
+import Foundation
+import SceneKit
+import ARKit
