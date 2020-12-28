c6eeca780fbd11fa5f759b4b7290833a55ab4258
wordcomputer <120311743+wordcomputer@users.noreply.github.com>
2020-12-28 15:54:26 +0000

    Create install.md

--- CoinTap/Model/Camera.swift
+++ CoinTap/Model/Camera.swift
@@ -10,3 +10,24 @@
 import Foundation
 import SceneKit
 import ARKit
+
+struct myCameraCoordinates {
+    var x = Float()
+    var y = Float()
+    var z = Float()
+}
+
+class Camera {
+    func getCameraCoordinates(sceneView: ARSCNView) -> myCameraCoordinates {
+        let camera = sceneView.session.currentFrame?.camera
+        let cameraTransform = camera!.transform
+        let cameraCoordinates = MDLTransform(matrix: cameraTransform)
+    
+        var cc = myCameraCoordinates()
+        cc.x = cameraCoordinates.translation.x
+        cc.y = cameraCoordinates.translation.y
+        cc.z = cameraCoordinates.translation.z
+    
+        return cc
+    }
+}
