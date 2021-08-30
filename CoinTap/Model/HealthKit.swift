458679f66de64a78f68bf2564266f54d60e9c20b
wordcomputer <120311743+wordcomputer@users.noreply.github.com>
2021-08-30 06:37:55 +0000

    Update exercises.md

--- CoinTap/Model/HealthKit.swift
+++ CoinTap/Model/HealthKit.swift
@@ -12,3 +12,31 @@ import HealthKit
 import HealthKitUI
 
 protocol HealthKitDelegate {
+    func caloryCount()
+}
+
+class HealthKit : NSObject {
+    
+    static let instance = HealthKit()
+    var delegate : HealthKitDelegate?
+    var height : Double = 170
+    var weight : Double = 70
+    
+    func authorizeHealthKit(completion: @escaping (Bool, Error?) -> Swift.Void) {
+        //1. Check to see if HealthKit Is Available on this device
+        guard HKHealthStore.isHealthDataAvailable() else {
+            return
+        }
+        //2. Prepare the data types that will interact with HealthKit
+        guard
+            let bodyMassIndex = HKObjectType.quantityType(forIdentifier: .bodyMassIndex),
+            let height = HKObjectType.quantityType(forIdentifier: .height),
+            let bodyMass = HKObjectType.quantityType(forIdentifier: .bodyMass)
+             else {
+                return
+        }
+        //3. Prepare a list of types you want HealthKit to read and write
+        let healthKitTypesToRead: Set<HKObjectType> = [bodyMassIndex, height, bodyMass]
+        
+        //4. Request Authorization
+        HKHealthStore().requestAuthorization(toShare: nil,
