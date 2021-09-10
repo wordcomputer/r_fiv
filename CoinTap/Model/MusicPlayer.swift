6059d3268fc9a4e5be2dd637aa9a9a1d91262935
wordcomputer <120311743+wordcomputer@users.noreply.github.com>
2021-09-10 10:18:10 +0000

    bump fix

--- CoinTap/Model/MusicPlayer.swift
+++ CoinTap/Model/MusicPlayer.swift
@@ -1,3 +1,25 @@
 
 //
 //  MusicPlayer.swift
+//  Rain
+//
+//  Created by Ridwan Abdurrasyid on 18/05/19.
+//  Copyright © 2019 Mentimun Mulus. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+
+class MusicPlayer :  NSObject{
+    static let instance = MusicPlayer()
+    var audioPlayer : AVAudioPlayer?
+
+    func playSound(){
+        let sound = Bundle.main.path(forResource: "coin", ofType: "mp3")
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
+        } catch {
+            print(error)
+        }
+        audioPlayer?.prepareToPlay()
