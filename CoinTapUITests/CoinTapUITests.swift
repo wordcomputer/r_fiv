7e555e377b0da38d52a2cba307f02a240cc450eb
wordcomputer <120311743+wordcomputer@users.noreply.github.com>
2022-05-25 09:11:00 +0000

    Update walkhub submodule

--- CoinTapUITests/CoinTapUITests.swift
+++ CoinTapUITests/CoinTapUITests.swift
@@ -11,4 +11,6 @@ import XCTest
 class CoinTapUITests: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    
