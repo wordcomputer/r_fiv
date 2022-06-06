1645b020b9d3c6c574a4254580ff239870c21a23
wordcomputer <120311743+wordcomputer@users.noreply.github.com>
2022-06-06 08:48:51 +0000

    3.3.9

--- CoinTapUITests/CoinTapUITests.swift
+++ CoinTapUITests/CoinTapUITests.swift
@@ -16,4 +16,4 @@ class CoinTapUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
-        // UI tests must launch the application that they test. Doing this in setup
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happ
