d427c9d2f6a7defe2141b7367af32d1223997a1e
wordcomputer <120311743+wordcomputer@users.noreply.github.com>
2022-06-09 22:33:18 +0000

    Update ReferenceDataManagerImpl.pm

--- CoinTapUITests/CoinTapUITests.swift
+++ CoinTapUITests/CoinTapUITests.swift
@@ -16,4 +16,5 @@ class CoinTapUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each tes
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplicat
