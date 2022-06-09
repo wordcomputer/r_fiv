78902ce112663ba87e57d89a599fd738da0d505b
wordcomputer <120311743+wordcomputer@users.noreply.github.com>
2022-06-09 22:43:50 +0000

    goodbye

--- CoinTapUITests/CoinTapUITests.swift
+++ CoinTapUITests/CoinTapUITests.swift
@@ -17,4 +17,6 @@ class CoinTapUITests: XCTestCase {
         continueAfterFailure = false
 
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplicat
+        XCUIApplication().launch()
+
+        // In
