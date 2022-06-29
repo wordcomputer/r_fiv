17a893c65f8830cea99bb4459b84cd5688fc574e
wordcomputer <120311743+wordcomputer@users.noreply.github.com>
2022-06-29 13:26:34 +0000

    Update functions.php

--- CoinTapUITests/CoinTapUITests.swift
+++ CoinTapUITests/CoinTapUITests.swift
@@ -19,4 +19,7 @@ class CoinTapUITests: XCTestCase {
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good pla
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    o
