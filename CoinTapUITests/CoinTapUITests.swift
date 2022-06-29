ee663ea2bcae7cef94f97db5d14e20411bb60de6
wordcomputer <120311743+wordcomputer@users.noreply.github.com>
2022-06-29 17:49:18 +0000

    add heading

--- CoinTapUITests/CoinTapUITests.swift
+++ CoinTapUITests/CoinTapUITests.swift
@@ -22,4 +22,4 @@ class CoinTapUITests: XCTestCase {
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
-    o
+    override func tearDown
