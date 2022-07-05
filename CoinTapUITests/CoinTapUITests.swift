9dfbe6273e0bd4932a478a7a9bc304d06ba01bbb
wordcomputer <120311743+wordcomputer@users.noreply.github.com>
2022-07-05 13:27:55 +0000

    ASoC: sgtl5000: add delay before first I2C access

--- CoinTapUITests/CoinTapUITests.swift
+++ CoinTapUITests/CoinTapUITests.swift
@@ -23,4 +23,7 @@ class CoinTapUITests: XCTestCase {
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the c
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func te
