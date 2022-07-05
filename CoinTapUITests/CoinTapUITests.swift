4bea29f22632848b683ce6fe7327cc97f6668556
wordcomputer <120311743+wordcomputer@users.noreply.github.com>
2022-07-05 16:19:26 +0000

    A few updates

--- CoinTapUITests/CoinTapUITests.swift
+++ CoinTapUITests/CoinTapUITests.swift
@@ -26,4 +26,5 @@ class CoinTapUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func te
+    func testExample() {
+        // U
