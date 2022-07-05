efd6228e64c5e5a58942e15840de88c7b17fb18f
wordcomputer <120311743+wordcomputer@users.noreply.github.com>
2022-07-05 05:57:24 +0000

    design fix

--- CoinTapUITests/CoinTapUITests.swift
+++ CoinTapUITests/CoinTapUITests.swift
@@ -23,4 +23,4 @@ class CoinTapUITests: XCTestCase {
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of
+        // Put teardown code here. This method is called after the invocation of each test method in the c
